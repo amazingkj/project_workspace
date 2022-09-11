eca3fb8dd6ff09366ef8cbcd459bc53ee88d7817
kim\kim <jiin724@gmail.com>
2022-09-11 23:27:41 +0900

    memberVo, DAO 복구

---
+++ MVC/src/net/golf/dao/MemberDAOImpl.java
@@ -0,0 +1,174 @@
+package net.golf.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import net.golf.vo.MemberVO;
+
+
+
+public class MemberDAOImpl {//회원관리 JDBC
+
+	Connection con=null;//DB연결
+	PreparedStatement pt=null; //쿼리문 수행
+	ResultSet rs=null;//검색 결과 레코드를 저장 rs
+	DataSource ds=null;//DBCP 커넥션풀 관리 ds
+	String sql=null;
+	
+	public MemberDAOImpl() {
+		try {
+			Context ctx=new InitialContext();
+			ds=(DataSource)ctx.lookup("java:comp/env/jdbc/xe");//server.xml의 xe를 JNDI
+			//디렉토리 서비스로 찾아서 커넥션 풀 관리 ds 생성
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}//생성자
+	
+	//아이디 중복검색
+	public MemberVO idCheck(String id) {
+		MemberVO m=null;
+		
+		try {
+			con=ds.getConnection();//커넥션 풀 관리 ds로 db연결 con생성
+			sql="select * from memberT where mem_id=?";
+			pt = con.prepareStatement(sql);//쿼리문을 미리 컴파일하여 수행할 pt생성
+			pt.setString(1,id);//쿼리문의 첫번째 물음표에 문자열로 아이디저장
+			rs=pt.executeQuery();//검색 쿼리문 수행후 결과레코드를 rs에 저장
+			
+			if(rs.next()) {//다음 레코드 행이 존재하면 참
+				m=new MemberVO();
+				m.setM_id(rs.getString("mem_id"));//mem_id컬럼으로 부터 문자열로 아이디값을
+				//가져와서 setter()메서드에 저장
+			}
+		}catch(Exception e) {e.printStackTrace();}
+		finally {
+			try {
+			    if(rs != null) rs.close();
+			    if(pt != null) pt.close();
+			    if(con != null) con.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return m;
+	}//idCheck()
+	
+	//회원저장
+	public int insertMember(MemberVO m) {
+		int re=-1;//삭제 실패시 반환값
+		
+		try {
+			con=ds.getConnection();
+			sql="insert into golformemberNew (m_id,m_pw,m_phone,m_email,m_gender,m_date,m_state)"
+					+ "values(?,?,?,?,?,sysdate,1)";
+
+			pt=con.prepareStatement(sql);
+			pt.setString(1,m.getM_id());
+			pt.setString(2,m.getM_pw());
+			pt.setString(3,m.getM_phone());
+			pt.setString(4,m.getM_email());
+			pt.setString(5,m.getM_gender());
+			pt.setString(6,m.getM_date());
+			pt.setInt(7,m.getM_state());
+			
+			re=pt.executeUpdate();//저장 쿼리문 수행후 성공한 레코드 행의 개수를 반환
+			
+		}catch(Exception e) {e.printStackTrace();}
+		finally {
+			try {
+			 if(pt != null) pt.close();
+			 if(con != null) con.close();
+			}catch(Exception e) {e.printStackTrace();}
+		}
+		return re;
+	}//insertMember()
+	
+
+	//비번찾기
+	public MemberVO pwdMember(MemberVO m) {
+		MemberVO pm=null;
+		
+		try{
+			con=ds.getConnection();
+			sql="select mem_pwd from memberT where m_id=? and m_name=?";
+			//회원아이디와 이름을 기준으로 오라클로부터 비번을 검색
+			pt=con.prepareStatement(sql);
+			pt.setString(1,m.getM_id());
+			pt.setString(2,m.getM_name());
+			rs=pt.executeQuery();
+			
+			if(rs.next()) {
+				pm=new MemberVO();
+				pm.setM_id(rs.getString("m_pw"));
+			}
+		}catch(Exception e) {e.printStackTrace();}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pt != null) pt.close();
+				if(con != null) con.close();
+			}catch(Exception e) {e.printStackTrace();}
+		}
+		return pm;
+	}//PwdMember()
+
+	//로그인 인증처리
+	public MemberVO loginCheck(String id) {
+		MemberVO m=null;
+		
+		try {
+			con=ds.getConnection();
+			sql="select * from golformemberNew where m_id=? and m_state=1";
+			//아이디와 가입회원 1인 경우만 로그인 인증
+			pt=con.prepareStatement(sql);//쿼리문을 미리 컴파일 해서 수행할 pt생성
+			pt.setString(1,id);
+			rs=pt.executeQuery();
+			
+			if(rs.next()) {
+				m=new MemberVO();
+				m.setM_id(rs.getString("m_id"));
+				m.setM_file(rs.getString("m_file"));//프로필 사진 경로를 저장
+			}
+		}catch(Exception e) {e.printStackTrace();}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pt != null) pt.close();
+				if(con != null) con.close();
+			}catch(Exception e) {e.printStackTrace();}
+		}
+		return m;
+	}//loginCheck()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MVC/src/net/golf/vo/MemberVO.java
@@ -0,0 +1,104 @@
+package net.golf.vo;
+
+public class MemberVO {//자바 저장 빈 클래스, 되도록이면 네임패러미터 이름과 빈클래스 변수명, 테이블 컬럼명을 같게 한다. 
+	//회원관리 데이터 저장 빈
+	
+	private int  m_no;
+	private String m_name;
+	private String m_id;
+	private String m_pw;
+	private String m_phone;
+	private String m_addr;
+	private String m_email;
+	private String m_gender;
+	private String m_file;
+	private String m_date;
+	private int m_state;
+	private String m_delcont;
+	private String m_deldate;
+	public int getM_no() {
+		return m_no;
+	}
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public String getM_addr() {
+		return m_addr;
+	}
+	public void setM_addr(String m_addr) {
+		this.m_addr = m_addr;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_gender() {
+		return m_gender;
+	}
+	public void setM_gender(String m_gender) {
+		this.m_gender = m_gender;
+	}
+	public String getM_file() {
+		return m_file;
+	}
+	public void setM_file(String m_file) {
+		this.m_file = m_file;
+	}
+	public String getM_date() {
+		return m_date;
+	}
+	public void setM_date(String m_date) {
+		this.m_date = m_date;
+	}
+	public int getM_state() {
+		return m_state;
+	}
+	public void setM_state(int m_state) {
+		this.m_state = m_state;
+	}
+	public String getM_delcont() {
+		return m_delcont;
+	}
+	public void setM_delcont(String m_delcont) {
+		this.m_delcont = m_delcont;
+	}
+	public String getM_deldate() {
+		return m_deldate;
+	}
+	public void setM_deldate(String m_deldate) {
+		this.m_deldate = m_deldate;
+	}
+	
+	
+	
+	
+	
+	
+
+}
