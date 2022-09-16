fb0463376e26f2ee1d43b53dbc1d9581fce77036
kim\kim <jiin724@gmail.com>
2022-09-16 21:16:04 +0900

    좋아요 기능 추가를 위한 sql 테이블 변경, b_like 추가

---
+++ MVC/WebContent/view/SQL/golforboard.sql
@@ -0,0 +1,45 @@
+--golforboard mvc 계층형 게시판 
+
+/*** 댓글하고 게시물 파일 첨부 때문에 테이블 다시 짜야함 */
+
+create table golforboard( --테이블 생성
+  b_no number(38) primary key --게시물 번호
+ ,b_name varchar2(100) not null --글쓴이
+ ,b_title varchar2(200) not null --글제목
+ ,b_pwd varchar2(50) not null --비번
+ ,b_cont varchar2(4000) not null --글내용
+ ,b_hit number(38) default 0 --조회수
+ ,b_ref number(38) --원본글과 관리자 답변글을 묶어주는 글 그룹번호값
+ ,b_step number(38) --원본글이면 0,첫번째 답변글이면 1,두번째 답변글이면 2,원본글과 답변글을 구분하는 번호값이면서 몇번째 답변글인가를 알려준다.
+ ,b_level number(38) --답변글 정렬순서
+ ,b_date date --글 등록날짜
+);
+
+
+alter table golforboard MODIFY b_pwd varchar2(50) NULL;
+--b_pwd 의 데이터 형식을 varchar(50) 으로 지정하고 NULL값을 허용한다.
+
+alter table golforboard add b_like number(38) default 0;
+
+select * from golforboard order by b_no desc;
+
+--boardT_no_seq 시퀀스 생성
+create sequence board_no_seq
+start with 1 --1부터 시작
+increment by 1 --1씩 증가
+nocache;
+
+-- 시퀀스 다음 번호값 확인
+select board_no_seq.nextval from dual;
+
+
+
+
+--게시물 입력 
+insert into golforboard (b_no,b_name,b_title,b_pwd,b_cont,b_ref,b_step,b_level,b_date) 
+VALUES (board_no_seq.nextval,'이름','제목','1111','내용입니다',board_no_seq.nextval,0,0,sysdate);
+
+insert into golforboard (b_no,b_name,b_title,b_pwd,b_cont,b_ref,b_step,b_level,b_date) 
+VALUES (board_no_seq.nextval,'이름2','제목2','1111','내용입니다2',board_no_seq.nextval,0,0,sysdate);
+
+
